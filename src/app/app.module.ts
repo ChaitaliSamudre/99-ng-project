e694489977339aefc84affb94d63b33b2ec9951f
Chaitali Samudre <chaitali_samudre@thbs.india.com>
2022-04-04 13:12:51 +0530

    added list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainAreaComponent } from './main-area/main-area.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    MainAreaComponent,
+    LoginComponent,
+    RegisterComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
